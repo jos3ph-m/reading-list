5f648c3b27ffeb89e94cca2d1ca14da82ec0ede8
Joseph M <jmedina1985@hotmail.com>
2022-08-11 12:36:38 -0700

    Firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: 'readinglistapp-c761b.firebaseapp.com',
+  projectId: 'readinglistapp-c761b',
+  storageBucket: 'readinglistapp-c761b.appspot.com',
+  messagingSenderId: '669047213390',
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+};
