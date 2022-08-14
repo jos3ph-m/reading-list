beda7153b2fb184823cf901dd5e77e64499f4b15
Joseph M <jmedina1985@hotmail.com>
2022-08-14 14:07:57 -0700

    Getting documents

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BookList from '../components/BookList';
 import BookForm from '../components/BookForm';
 
 export default function Home() {
   const [books, setBooks] = useState(null);
+
+  useEffect();
 
   return (
     <div>
