fd3e01a02b8db9b017fd3bfddd05ba870df1b0fd
Joseph M <jmedina1985@hotmail.com>
2022-08-14 14:08:52 -0700

    Getting documents

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import BookList from '../components/BookList';
 import BookForm from '../components/BookForm';
 
+import { db } from '../firebase/config';
+
 export default function Home() {
   const [books, setBooks] = useState(null);
 
