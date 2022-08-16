90ec69c1642a1388dee92bf0dd7e80153a423cd5
Joseph M <jmedina1985@hotmail.com>
2022-08-16 10:51:42 -0700

    Real-time colleciton data

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import { db } from '../firebase.config';
 
+// firebase imports
+import { collection, onSnapshot } from 'firebase/firestore';
+
 export const useCollection = () => {
   const [documents, setDocuments] = useState(null);
 
