67a490c32e2ad9ae3cc3f62da3791f0d288b8270
Joseph M <jmedina1985@hotmail.com>
2022-08-18 15:24:28 -0700

    singing users up

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -3,7 +3,9 @@ import { useState } from 'react';
 export const useSignup = () => {
   const [error, setError] = useState(null);
 
-  const signup = (email, password) => {};
+  const signup = (email, password) => {
+    setError(null);
+  };
 
   return { error, signup };
 };
