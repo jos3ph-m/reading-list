85d8b4e34e8fda58c7bbd9c03958c769cc3e9a44
Joseph M <jmedina1985@hotmail.com>
2022-08-18 15:25:22 -0700

    singing users up

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 // firebase imports
 import { auth } from '../firebase/config';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
