4e4c7f60427f933fb729fd7b2ac0505d3757e92a
Joseph M <jmedina1985@hotmail.com>
2022-08-29 14:05:33 -0700

    Adding auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -5,5 +5,7 @@ export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
   switch (action.type) {
+    case 'LOGIN':
+      return { ...state, user: action.payload };
   }
 };
