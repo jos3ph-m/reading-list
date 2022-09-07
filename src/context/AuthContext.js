6987a8aefa6a2980475e06f7c93c655f676c2fa1
Joseph M <jmedina1985@hotmail.com>
2022-09-06 20:05:06 -0700

    slight adjustments

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -19,7 +19,7 @@ export const authReducer = (state, action) => {
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
-    user: state.AuthContext,
+    user: null,
     authIsReady: false,
   });
 
