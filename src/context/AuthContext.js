db294e8a6802365ca6f6555b52ba56ba4f0b83d0
Joseph M <jmedina1985@hotmail.com>
2022-09-07 20:22:55 -0700

    slight adjustments

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -34,7 +34,7 @@ export const AuthContextProvider = ({ children }) => {
 
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
-      {children}
+      {children.dispatch}
     </AuthContext.Provider>
   );
 };
