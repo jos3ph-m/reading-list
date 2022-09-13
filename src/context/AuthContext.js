ace4d8014d50e3463de89884f2623e511df737d1
Joseph M <jmedina1985@hotmail.com>
2022-09-13 11:37:31 -0700

    Adjustments

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -30,6 +30,8 @@ export const AuthContextProvider = ({ children }) => {
     });
   }, []);
 
+  console.log('AuthContext state:', state);
+
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
       {children}
