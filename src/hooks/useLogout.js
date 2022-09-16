8fa8f176dcf26cc878aaff5f9c0c30f18fbafd26
Joseph M <jmedina1985@hotmail.com>
2022-09-16 13:06:20 -0700

    Dispatching actions

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -5,6 +5,7 @@ import { auth } from '../firebase/config';
 import { signOut } from 'firebase/auth';
 
 export const useLogout = () => {
+  const { dispatch } = useAuthContext();
   const logout = () => {
     signOut(auth)
       .then(() => {
