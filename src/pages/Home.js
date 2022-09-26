45e32cf1e53abd1b186c90de73de331f6bcb2b0a
Joseph M <jmedina1985@hotmail.com>
2022-09-26 10:50:04 -0700

    Firestore queries

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { useCollection } from '../hooks/useCollection';
 import { useAuthContext } from '../hooks/useAuthContext';
 
 export default function Home() {
+  const { user } = useAuthContext();
   const { documents: books } = useCollection('books', ['uid', '==', user.uid]);
   return (
     <div>
