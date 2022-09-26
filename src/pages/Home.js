fa1fcd9474e5949f9bd7d1f836f97979c623d06b
Joseph M <jmedina1985@hotmail.com>
2022-09-26 10:49:53 -0700

    Firestore queries

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import BookList from '../components/BookList';
 import BookForm from '../components/BookForm';
 import { useCollection } from '../hooks/useCollection';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 export default function Home() {
   const { documents: books } = useCollection('books', ['uid', '==', user.uid]);
