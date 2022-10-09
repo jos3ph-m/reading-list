140347d53ab1150593446d29be68daac58a6db8a
Joseph M <jmedina1985@hotmail.com>
2022-10-08 18:18:17 -0700

    Adjustments

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -11,7 +11,7 @@ export const useCollection = (c, _q) => {
   const q = useRef(_q);
 
   useEffect(() => {
-    let ref = collection();
+    let ref = collection(db);
 
     const unsub = onSnapshot(ref, (snapshot) => {
       let results = [];
