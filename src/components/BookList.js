170debf48a9e831b517da1a639b4095e6db110fc
Joseph M <jmedina1985@hotmail.com>
2022-10-18 15:37:52 -0700

    Adjustments

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -12,9 +12,7 @@ export default function BookList({ books }) {
     <div className="book-list">
       <ul>
         {books.map((book) => (
-          <li key={book.id} onClick={() => handleClick(book.id)}>
-            {book.title}
-          </li>
+          <li key={book.id} onClick={() => handleClick(book.id)}></li>
         ))}
       </ul>
     </div>
