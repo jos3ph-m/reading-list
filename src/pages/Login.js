e0cd6f27b7ff2e0078ad0277193b1ad8e3c1dc44
Joseph M <jmedina1985@hotmail.com>
2022-10-22 23:19:57 -0700

    Adjustments

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -19,7 +19,7 @@ export default function Login() {
           <span>email:</span>
           <input
             required
-            type="name"
+            type="email"
             onChange={(e) => setEmail(e.target.value)}
             value={email}
           />
