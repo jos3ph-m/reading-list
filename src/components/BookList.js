6cecf0c8944f5f078a2a004ff74adeb6dc487421
Joseph M <jmedina1985@hotmail.com>
2022-11-08 19:43:27 -0800

    BookList adjustment

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -5,6 +5,7 @@ import { doc, deleteDoc } from 'firebase/firestore';
 export default function BookList({ books }) {
   const handleClick = async (id) => {
     const docRef = doc(db, 'books', id);
+    await deleteDoc(docRef);
   };
 
   return (
